12cc24701c2e4cc388b211ae57780f8231d42f92
dhanalakshmi <dhanalakshmi@dhanalakshmis-MacBook-Air.local>
2019-05-04 14:05:02 +0530

    First commit

---
+++ Strayer/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Strayer
+//
+//  Created by dhanalakshmi on 04/05/19.
+//  Copyright © 2019 edu.strayer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello world")
+    }
+
+
+}
+
